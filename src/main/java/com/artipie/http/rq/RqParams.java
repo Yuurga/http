ae2bf19b777b4135767aa71441c925390e9b3cd8
d.koroleva <d.koroleva@deltasolutions.ru>
2021-12-29 22:17:04 +0300

    feat: add new method RqParams.findValues that return all parameter values that found in query (#413)

--- src/main/java/com/artipie/http/rq/RqParams.java
+++ src/main/java/com/artipie/http/rq/RqParams.java
@@ -5,10 +5,14 @@
 package com.artipie.http.rq;
 
 import com.google.common.base.Splitter;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLDecoder;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -51,14 +55,35 @@ public final class RqParams {
      * @return Parameter value.
      */
     public Optional<String> value(final String name) {
-        final Optional<String> result;
         if (this.query == null) {
-            result = Optional.empty();
+            return Optional.empty();
         } else {
-            result = StreamSupport.stream(
+            return findValues(name).findFirst();
+        }
+    }
+
+    /**
+     * Get values for parameter value by name.
+     * Empty {@link List} is returned if parameter not found.
+     * Return List with all founded values if parameters with same name present in query
+     *
+     * @param name Parameter name.
+     * @return List of Parameter values
+     */
+    public List<? extends String> values(final String name) {
+        if (this.query == null) {
+            return Collections.emptyList();
+        } else {
+            return findValues(name).collect(Collectors.toList());
+        }
+    }
+
+
+    private Stream<String> findValues(String name) {
+        return StreamSupport.stream(
                 Splitter.on("&").omitEmptyStrings().split(this.query).spliterator(),
                 false
-            ).flatMap(
+        ).flatMap(
                 param -> {
                     final String prefix = String.format("%s=", name);
                     final Stream<String> value;
@@ -69,13 +94,13 @@ public final class RqParams {
                     }
                     return value;
                 }
-            ).findFirst();
-        }
-        return result.map(RqParams::decode);
+        ).map(RqParams::decode);
     }
+
 
     /**
      * Decode string using URL-encoding.
+     *
      * @param enc Encoded string
      * @return Decoded string
      */
--- src/test/java/com/artipie/http/rq/RqParamsTest.java
+++ src/test/java/com/artipie/http/rq/RqParamsTest.java
@@ -4,11 +4,17 @@
  */
 package com.artipie.http.rq;
 
-import java.util.Optional;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * Tests for {@link RqParams}.
@@ -19,18 +25,45 @@ public final class RqParamsTest {
 
     @ParameterizedTest
     @CsvSource({
-        ",",
-        "'',",
-        "some=param,",
-        "foo=bar,bar",
-        "foo=,''",
-        "some=param&foo=123,123",
-        "foo=bar&foo=baz,bar"
+            ",",
+            "'',",
+            "some=param,",
+            "foo=bar,bar",
+            "foo=,''",
+            "some=param&foo=123,123",
+            "foo=bar&foo=baz,bar",
+            "foo=bar%26bobo,bar&bobo"
     })
     void findsParamValue(final String query, final String expected) {
         MatcherAssert.assertThat(
-            new RqParams(query).value("foo"),
-            new IsEqual<>(Optional.ofNullable(expected))
+                new RqParams(query).value("foo"),
+                new IsEqual<>(Optional.ofNullable(expected))
         );
     }
+
+
+    static Stream<Arguments> stringQueryAndListOfParamValues() {
+        return Stream.of(
+                Arguments.arguments("", Collections.emptyList()),
+                Arguments.arguments("''", Collections.emptyList()),
+                Arguments.arguments("foo=", List.of("")),
+                Arguments.arguments("prm=koko", Collections.emptyList()),
+                Arguments.arguments("fo=bar&fy=baz", Collections.emptyList()),
+                Arguments.arguments("foo=bar&fyi=baz", List.of("bar")),
+                Arguments.arguments("foo=bar&foo=baz",List.of("bar","baz")),
+                Arguments.arguments("foo=bar&key=ksu&foo=bobo",List.of("bar","bobo")),
+                Arguments.arguments("foo=bar%26bobo",List.of("bar&bobo"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringQueryAndListOfParamValues")
+    void findsParamValues(final String query, final List<String> expected) {
+        MatcherAssert.assertThat(
+                new RqParams(query).values("foo"),
+                new IsEqual<>(expected)
+        );
+    }
+
+
 }
